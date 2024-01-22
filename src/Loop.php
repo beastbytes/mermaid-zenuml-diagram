78791953211d4d0f17f355db79ae4ca2bdf3c4f4
Chris Yates <chris@clyates.plus.com>
2024-01-22 16:36:31 +0000

    Remove render() and CommentTrait

--- src/Loop.php
+++ src/Loop.php
@@ -11,20 +11,9 @@ namespace BeastBytes\Mermaid\ZenumlDiagram;
 /** @link https://zenuml.com/docs/language-guide/loops/ */
 final class Loop extends ConditionalBlock
 {
-    use CommentTrait;
-
     public function __construct(string $condition, LoopType $type = LoopType::Loop)
     {
         parent::__construct($condition);
         $this->type = $type->value;
-    }
-
-    /** @internal */
-    public function render(string $indentation): string
-    {
-        $output = [];
-        $this->renderComment($indentation, $output);
-        $this->renderBlock($indentation, $output);
-        return implode("\n", $output);
     }
 }
